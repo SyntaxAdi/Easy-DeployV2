991ab4093e9e744bb9e35fe86a1aa9e05ca52fd6
SyntaxAdi <aadityapawar8209@gmail.com>
2026-06-28 13:52:12 +0530

    refactor update script to clear terminal before completion message

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,4 +8,5 @@ sudo apt update
 echo "[+] Upgrading system packages..."
 sudo apt upgrade -y
 
-echo "[+] Package update completed successfully."
+clear
+echo "Package update completed successfully."
